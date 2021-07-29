2e79c0b41ad68daf6a48d17f511a3c8eef8ee377
soos <62181284+sooswastaken@users.noreply.github.com>
2021-07-28 21:00:51 -0400

    v0.1

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useRef, useState} from "react";
+
+function App() {
+  const search_bar = useRef()
+  const [searched, setSearched] = useState(undefined)
+
+
+
+  const search_bar_enter=(event)=> {
+    if (event.key === "Enter") {
+      search(false)
+      document.activeElement.blur()
+    }
+}
+  function search(isFromButtonPress) {
+    if (isFromButtonPress) return setSearched(search_bar.current.value)
+    if (document.activeElement === search_bar.current || !search_bar.current.value.length === 0) {
+      setSearched(search_bar.current.value)
+      
+    }
+  }
+
+
+  return (
+    <>
+    <input className="search" ref={search_bar} type="text" placeholder="Search for a subreddit" onKeyPress={(e) => search_bar_enter(e) }/>
+    <button className="search-button" onClick={() => search(true)}>Search</button>
+    <p className="searched">{"Searching for r/"+searched}</p>
+    </>
+  );
+}
+
+export default App;
